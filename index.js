6d853ad728cd6cef91293e28d18a7cb57bce02cc
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-04-02 09:44:32 -0400

    switch host

--- index.js
+++ index.js
@@ -3,4 +3,27 @@ document.addEventListener("DOMContentLoaded", function() {
   init();
 
   // ADD CODE HERE!
+
+  // make an array of the things we want to capture
+  let arrowKeys = ['ArrowRight', 'ArrowLeft', 'ArrowUp', 'ArrowDown'];
+
+  // capture keypress event and delegate to function
+  document.addEventListener('keydown', function(event){
+    if (arrowKeys.includes(event.key)){
+
+      event.preventDefault();
+      createMoveItem(event.key);
+
+    } else {
+
+      console.log('some other key');
+
+    }
+   
+  });
+
+  function createMoveItem(eventKey){
+    
+  }
+
 });
